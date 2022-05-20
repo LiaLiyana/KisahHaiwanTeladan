def7045a8c635a4163b815a3bd09b8e22e40ac5a
LiaLiyana <nurliyanamuty@gmail.com>
2022-05-20 12:25:05 +0800

    Loop json images

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,63 +1,23 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Image, Button} from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet, View, Text, Image,TouchableOpacity} from "react-native";
+import Icons from './Icons';
+import resources from './resources.json';
 
-const Square = ({ backgroundColor }) => {
-  const [height, setHeight] = useState(0);
-  return (
-    <View
-      onLayout={(e) => setHeight(e.nativeEvent.layout.width)}
-      style={{ flex: 1, height, backgroundColor }}
-    />
-  );
-};
 
 export default class ContentPage extends React.Component {
   render() {
     return (
-      <View style = {styles.container}>
-        <View style={{ flexDirection: "column"}}>
-            <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity onPress={() => this.props.pageChange(3)}>
-              <Image
-                source={require('../assets/images/rabbit-g521c44c12_640.png')}
-                  style={{ width: 50, height: 50, margin: 16 }}
-                />
-            </TouchableOpacity>
-            <Text style={{ margin: 20 }}>Kisah Arnab dan Kura Kura </Text>
-            <Text  style={{ margin: 16 }}>01 </Text>
-          </View>
-          <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity onPress={() => this.props.pageChange(4)}>
-              <Image
-                source={require('../assets/images/penguin_640.jpg')}
-                  style={{ width: 50, height: 50, margin: 16 }}
-                />
-            </TouchableOpacity>
-            <Text style={{ margin: 20 }}>Kisah Semut dan Burung Merpati </Text>
-            <Text  style={{ margin: 16 }}>02</Text>
-          </View>
-          <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity onPress={() => this.props.pageChange(5)}>
-            <Image
-                source={require('../assets/images/dog_640.jpg')}
-                style={{ width: 50, height: 50, margin: 16 }}
-              />
-          </TouchableOpacity>
-            <Text style={{ margin: 20 }}>Kisah Anjing yang Tamak </Text>
-            <Text  style={{margin: 16 }}>03</Text>
-          </View>
-          <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity onPress={() => this.props.pageChange(6)}>
-              <Image
-                  source={require('../assets/images/lion_640.jpg')}
-                  style={{ width: 50, height: 50, margin: 16 }}
-                />
-            </TouchableOpacity>
-            <Text style={{ margin: 20 }}>Kisah Singa dan Arnab </Text>
-            <Text  style={{margin: 16 }}>04</Text>
-          </View>
-        </View>
+      <View style={styles.container}>
+          {resources.map((item) =>
+          <View style={{ flexDirection: "column"}} key={item.id}>
+              <View style={{ flexDirection: "row" }}>
+                  <TouchableOpacity onPress={() => this.props.pageChange(3)}>
+                  <Image style={{ width: 50, height: 50, margin: 16 }} source={Icons[item.image]} />
+                  </TouchableOpacity>
+                  <Text style={{ margin: 20 }}>{item.title} </Text>
+                  <Text  style={{ margin: 16 }}>{item.numbering} </Text>
+              </View>
+          </View>)}
       </View>
     );
   }
---
+++ src/components/Icons.js
@@ -0,0 +1,4 @@
+exports.CAC = require('../assets/images/rabbit-g521c44c12_640.png');
+exports.CAD = require('../assets/images/penguin_640.jpg');
+exports.CAE = require('../assets/images/lion_640.jpg');
+exports.CAF = require('../assets/images/lion_640.jpg');
